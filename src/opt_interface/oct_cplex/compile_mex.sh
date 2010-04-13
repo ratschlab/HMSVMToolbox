e4c63e266cfebe77b9bf31ec3caa595e6759ced7
raetsch <raetsch@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2010-04-13 19:07:28 +0000

    minor changes

--- src/opt_interface/oct_cplex/compile_mex.sh
+++ src/opt_interface/oct_cplex/compile_mex.sh
@@ -4,6 +4,10 @@
 setenv CPLEX_LIB /fml/ag-raetsch/share/software/ilog/cplex90/lib/x86-64_RHEL3.0_3.2/static_pic/
 setenv CPLEX_INC /fml/ag-raetsch/share/software/ilog/cplex90/include/ilcplex
 
+echo CPLEX paths:
+echo $CPLEX_LIB
+echo $CPLEX_INC
+
 #ensure mkoctfile is in your $PATH or insert its full path here
 mkoctfile --mex -I$CPLEX_INC -L$CPLEX_LIB -lcplex -lpthread cplex_init_quit.c
 mkoctfile --mex -I$CPLEX_INC -L$CPLEX_LIB -lcplex -lpthread opt_set_param.c
